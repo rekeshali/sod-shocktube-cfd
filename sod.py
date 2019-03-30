6580d33a8a2fe517c203b54a56c5590eef4df0e5
rekesh <rekeshali@gmail.com>
2019-03-30 00:04:08 -0400

    changing plot style

--- sod.py
+++ sod.py
@@ -12,8 +12,8 @@ sys.path.append(outdir)
 ############################ INPUTS #############################
 #################################################################
 method  = 'hll' # steger-warming, roe, or hll
-dx  = 0.0025
-cfl = 0.499
+dx  = 0.005
+cfl = 0.4
 outfile = method + '-dx' + str(dx) + '-cfl' + str(cfl) + '.sod'
 #################################################################
 #################################################################
@@ -28,5 +28,5 @@ print('Completed in ' + str(time.time() - start) + ' s')
 sod = Sod(outdir + outfile) # Read the output file
 # sod.plot() # View plot of final tstep
 # sod.gif () # View gif of soltuion
-sod.plot(save=outdir + 'plots/' + outfile[:-4]+'.png') # Save plot of final tstep
-sod.gif (save=outdir + 'gifs/'  + outfile[:-4]+'.gif') # Gif results and view or save
+# sod.plot(save=outdir + 'plots/' + outfile[:-4]+'.png') # Save plot of final tstep
+# sod.gif (save=outdir + 'gifs/'  + outfile[:-4]+'.gif') # Gif results and view or save
